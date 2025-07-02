4499a8c14fc41cd00607b550a500ddaa438c6c46
Dan Speckhard-Pasque <pasquedan@gmail.com>
2025-07-02 13:35:22 -0500

    Finish the ETL process

---
+++ server/db/etl-scripts/helpers/brainvar.ts
@@ -0,0 +1,203 @@
+import { parse } from "csv-parse/sync";
+import type { Knex } from "knex";
+
+const DB_BATCH_SIZE = 1000;
+
+function parseTsv(tsvContent: string) {
+  return parse(tsvContent, {
+    delimiter: "\t",
+    columns: true,
+    skip_empty_lines: true,
+  });
+}
+
+interface GeneMapRow {
+  symbol: string;
+  ensembl_gene_id: string;
+  name: string;
+}
+
+interface MetaRow {
+  Braincode: string;
+  AgeDays: string;
+  sex: string;
+}
+
+interface CpmRow {
+  gene: string;
+  [sampleCode: string]: string;
+}
+
+export async function parseAndLoadBrainvarData({
+  db,
+  geneMapTsv,
+  cpmTsv,
+  metaTsv,
+  datasetId,
+}: {
+  db: Knex;
+  geneMapTsv: string;
+  cpmTsv: string;
+  metaTsv: string;
+  datasetId: string;
+}) {
+  const geneMapRows: GeneMapRow[] = parseTsv(geneMapTsv);
+  const metaRows: MetaRow[] = parseTsv(metaTsv);
+  if (!cpmTsv.startsWith("gene\t")) {
+    // csv-parse doesn't handle headless index columns like pandas, so we need to prepend
+    cpmTsv = "gene\t" + cpmTsv;
+  }
+  const cpmRows: CpmRow[] = parseTsv(cpmTsv);
+
+  const geneDbIdBySymbol = await parseAndLoadGeneMapData({ db, geneMapRows });
+  const sampleDbIdByCode = await parseAndLoadSampleMetadata({
+    db,
+    metaRows,
+    datasetId,
+  });
+  await parseAndLoadExpressionCpmData({
+    db,
+    geneDbIdBySymbol,
+    sampleDbIdByCode,
+    cpmRows,
+  });
+}
+
+async function parseAndLoadGeneMapData({
+  db,
+  geneMapRows,
+}: {
+  db: Knex;
+  geneMapRows: GeneMapRow[];
+}) {
+  const geneInsertRows = geneMapRows.map((row) => ({
+    symbol: row.symbol.toUpperCase(),
+    ensembl_id:
+      row.ensembl_gene_id && row.ensembl_gene_id !== "."
+        ? row.ensembl_gene_id
+        : null,
+    name: row.name,
+  }));
+
+  console.log(`Inserting ${geneInsertRows.length} gene mapping rows...`);
+  for (let i = 0; i < geneInsertRows.length; i += DB_BATCH_SIZE) {
+    const batch = geneInsertRows.slice(i, i + DB_BATCH_SIZE);
+    await db("genes").insert(batch).onConflict("symbol").ignore();
+  }
+
+  const allGenes = await db("genes").select("id", "symbol");
+  const geneDbIdBySymbol: { [key: string]: number } = {};
+  for (const gene of allGenes) {
+    geneDbIdBySymbol[gene.symbol] = gene.id;
+  }
+
+  return geneDbIdBySymbol;
+}
+
+async function parseAndLoadSampleMetadata({
+  db,
+  metaRows,
+  datasetId,
+}: {
+  db: Knex;
+  metaRows: MetaRow[];
+  datasetId: string;
+}) {
+  const metaInsertRows = metaRows.map((row) => ({
+    sample_code: row.Braincode,
+    dataset_id: datasetId,
+    age_days: parseInt(row.AgeDays),
+    sex: row.sex,
+  }));
+
+  console.log(`Inserting ${metaInsertRows.length} sample metadata rows...`);
+  for (let i = 0; i < metaRows.length; i += DB_BATCH_SIZE) {
+    const batch = metaInsertRows.slice(i, i + DB_BATCH_SIZE);
+    await db("samples").insert(batch).onConflict("sample_code").ignore();
+  }
+
+  const allSamplesForDataset = await db("samples")
+    .select("id", "sample_code")
+    .where("dataset_id", datasetId);
+  const sampleDbIdByCode: { [key: string]: number } = {};
+  for (const sample of allSamplesForDataset) {
+    sampleDbIdByCode[sample.sample_code] = sample.id;
+  }
+
+  return sampleDbIdByCode;
+}
+
+async function parseAndLoadExpressionCpmData({
+  db,
+  geneDbIdBySymbol,
+  sampleDbIdByCode,
+  cpmRows,
+}: {
+  db: Knex;
+  geneDbIdBySymbol: { [key: string]: number };
+  sampleDbIdByCode: { [key: string]: number };
+  cpmRows: CpmRow[];
+}) {
+  const cpmInsertRows = [];
+  const geneMissingSymbol: string[] = [];
+  const genesNotInDb: string[] = [];
+  const samplesNotInDb: string[] = [];
+  for (const row of cpmRows) {
+    const geneSymbol = row.gene.split("|")[1]?.toUpperCase();
+    if (!geneSymbol) {
+      geneMissingSymbol.push(row.gene);
+      continue;
+    }
+
+    const geneDbId = geneDbIdBySymbol[geneSymbol];
+    if (!geneDbId) {
+      genesNotInDb.push(row.gene);
+      continue;
+    }
+
+    const allSampleEntriesForGene = Object.entries(row).filter(
+      ([key]) => key !== "gene"
+    );
+
+    for (const [sampleCode, cmpValue] of allSampleEntriesForGene) {
+      const sampleDbId = sampleDbIdByCode[sampleCode];
+      if (!sampleDbId) {
+        samplesNotInDb.push(sampleCode);
+        continue;
+      }
+
+      cpmInsertRows.push({
+        gene_id: geneDbId,
+        sample_id: sampleDbId,
+        cpm: parseFloat(cmpValue),
+      });
+    }
+  }
+
+  if (geneMissingSymbol.length > 0) {
+    console.warn(
+      `[WARNING] ${geneMissingSymbol.length} genes were missing symbols and were skipped.`
+    );
+  }
+  if (genesNotInDb.length > 0) {
+    console.warn(
+      `[WARNING] ${genesNotInDb.length} genes were not found in DB and were skipped.`
+    );
+  }
+  if (samplesNotInDb.length > 0) {
+    console.warn(
+      `[WARNING]${samplesNotInDb.length} samples were not found in DB and were skipped.`
+    );
+  }
+
+  console.log(
+    `Inserting ${cpmInsertRows.length} gene expression rows... (This may take a while)`
+  );
+  for (let i = 0; i < cpmInsertRows.length; i += DB_BATCH_SIZE) {
+    const batch = cpmInsertRows.slice(i, i + DB_BATCH_SIZE);
+    await db("gene_expressions")
+      .insert(batch)
+      .onConflict(["gene_id", "sample_id"])
+      .ignore();
+  }
+}
--- server/db/etl-scripts/load-brainvar-fs.ts
+++ server/db/etl-scripts/load-brainvar-fs.ts
@@ -2,14 +2,12 @@
 
 import fs from "fs/promises";
 import zlib from "zlib";
-import { parse, type Options } from "csv-parse/sync";
 import process from "process";
 import knex from "knex";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import dbConfig from "../../knexfile.ts";
-
-const db = knex(dbConfig[process.env.NODE_ENV || "development"]);
+import { parseAndLoadBrainvarData } from "./helpers/brainvar.ts";
 
 function getDefaultDatasetId() {
   const now = new Date();
@@ -17,43 +15,6 @@ function getDefaultDatasetId() {
   const dd = String(now.getUTCDate()).padStart(2, "0");
   const yyyy = now.getUTCFullYear();
   return `brainvar-imported-${mm}${dd}${yyyy}`;
-}
-
-const argv = yargs(hideBin(process.argv))
-  .option("gene-map", {
-    type: "string",
-    demandOption: true,
-    describe: "Path to a gzipped gene map file",
-  })
-  .option("cpm", {
-    type: "string",
-    demandOption: true,
-    describe: "Path to CPM file",
-  })
-  .option("meta", {
-    type: "string",
-    demandOption: true,
-    describe: "Path to metadata file",
-  })
-  .option("dataset-id", {
-    type: "string",
-    demandOption: false,
-    describe: "Optional dataset identifier for this import",
-    default: getDefaultDatasetId(),
-  })
-  .help().argv;
-
-const GENE_MAP_FILE = argv["gene-map"];
-const CPM_FILE = argv["cpm"];
-const META_FILE = argv["meta"];
-const DATASET_ID = argv["dataset-id"];
-
-function parseTsv(tsvContent: string) {
-  return parse(tsvContent, {
-    delimiter: "\t",
-    columns: true,
-    skip_empty_lines: true,
-  });
 }
 
 async function readGzippedTsvFile(filePath: string) {
@@ -65,160 +26,60 @@ async function readTsvFile(filePath: string) {
   return await fs.readFile(filePath, "utf-8");
 }
 
-interface GeneMapRow {
-  symbol: string;
-  ensembl_gene_id: string;
-  name: string;
-}
-
-interface MetaRow {
-  Braincode: string;
-  AgeDays: string;
-  sex: string;
-}
-
-interface CpmRow {
-  gene: string;
-  [sampleCode: string]: string;
-}
-
-async function parseAndLoadBrainvarData({
-  geneMapTsv,
-  cpmTsv,
-  metaTsv,
-  datasetId,
-}: {
-  geneMapTsv: string;
-  cpmTsv: string;
-  metaTsv: string;
-  datasetId: string;
-}) {
-  const geneMapRows: GeneMapRow[] = parseTsv(geneMapTsv);
-  const metaRows: MetaRow[] = parseTsv(metaTsv);
-  if (!cpmTsv.startsWith("gene\t")) {
-    // csv-parse doesn't handle headless index columns like pandas, so we need to prepend
-    cpmTsv = "gene\t" + cpmTsv;
-  }
-  const cpmRows: CpmRow[] = parseTsv(cpmTsv);
-  // for (const row of cpmRows) {
-  //   // Strip down to just Ensembl ID if symbol is present.
-  //   row.gene = row.gene.split("|")[0];
-  // }
-
-  const geneInsertRows = geneMapRows.map((row) => ({
-    symbol: row.symbol.toUpperCase(),
-    ensembl_id:
-      row.ensembl_gene_id && row.ensembl_gene_id !== "."
-        ? row.ensembl_gene_id
-        : null,
-    name: row.name,
-  }));
-
-  const DB_BATCH_SIZE = 500;
-
-  for (let i = 0; i < geneInsertRows.length; i += DB_BATCH_SIZE) {
-    const batch = geneInsertRows.slice(i, i + DB_BATCH_SIZE);
-    await db("genes").insert(batch).onConflict("symbol").merge();
-  }
-
-  const allGenes = await db("genes").select("id", "symbol");
-  const geneDbIdBySymbol: { [key: string]: string } = {};
-  for (const gene of allGenes) {
-    geneDbIdBySymbol[gene.symbol] = gene.id;
-  }
-
-  const metaInsertRows = metaRows.map((row) => ({
-    sample_code: row.Braincode,
-    dataset_id: datasetId,
-    age_days: parseInt(row.AgeDays),
-    sex: row.sex,
-  }));
-  for (let i = 0; i < metaRows.length; i += DB_BATCH_SIZE) {
-    const batch = metaInsertRows.slice(i, i + DB_BATCH_SIZE);
-    await db("samples").insert(batch).onConflict("sample_code").merge();
-  }
-
-  const allSamples = await db("samples").select("id", "sample_code");
-  const sampleDbIdByCode: { [key: string]: string } = {};
-  for (const sample of allSamples) {
-    sampleDbIdByCode[sample.sample_code] = sample.id;
-  }
-
-  const cpmInsertRows = [];
-  const geneMissingSymbol: string[] = [];
-  const genesNotInDb: string[] = [];
-  const samplesNotInDb: string[] = [];
-  for (const row of cpmRows) {
-    const geneSymbol = row.gene.split("|")[1]?.toUpperCase();
-    if (!geneSymbol) {
-      geneMissingSymbol.push(row.gene);
-      continue;
-    }
-
-    const geneDbId = geneDbIdBySymbol[geneSymbol];
-    if (!geneDbId) {
-      genesNotInDb.push(row.gene);
-      continue;
-    }
-
-    const allSampleEntriesForGene = Object.entries(row).filter(
-      ([key]) => key !== "gene"
-    );
-
-    for (const [sampleCode, cmpValue] of allSampleEntriesForGene) {
-      const sampleDbId = sampleDbIdByCode[sampleCode];
-      if (!sampleDbId) {
-        samplesNotInDb.push(sampleCode);
-        continue;
-      }
-
-      cpmInsertRows.push({
-        gene_id: geneDbId,
-        sample_id: sampleDbId,
-        cpm: parseFloat(cmpValue),
-      });
-    }
-  }
-
-  if (geneMissingSymbol.length > 0) {
-    console.warn(
-      `${geneMissingSymbol.length} genes were missing symbols and were skipped:`,
-      geneMissingSymbol
-    );
-  }
-  if (genesNotInDb.length > 0) {
-    console.warn(
-      `${genesNotInDb.length} genes were not found in DB and were skipped:`,
-      genesNotInDb
-    );
-  }
-  if (samplesNotInDb.length > 0) {
-    console.warn(
-      `${samplesNotInDb.length} samples were not found in DB and were skipped:`,
-      samplesNotInDb
-    );
-  }
-
-  for (let i = 0; i < cpmInsertRows.length; i += DB_BATCH_SIZE) {
-    const batch = cpmInsertRows.slice(i, i + DB_BATCH_SIZE);
-    await db("gene_expressions")
-      .insert(batch)
-      .onConflict(["gene_id", "sample_id"])
-      .merge();
-  }
-}
-
 async function run() {
-  const geneMapTsv = await readGzippedTsvFile(GENE_MAP_FILE);
-  const cpmTsv = await readTsvFile(CPM_FILE);
-  const metaTsv = await readTsvFile(META_FILE);
+  const argv = yargs(hideBin(process.argv))
+    .option("gene-map", {
+      type: "string",
+      demandOption: true,
+      describe: "Path to a gzipped gene map file",
+    })
+    .option("cpm", {
+      type: "string",
+      demandOption: true,
+      describe: "Path to CPM file",
+    })
+    .option("meta", {
+      type: "string",
+      demandOption: true,
+      describe: "Path to metadata file",
+    })
+    .option("dataset-id", {
+      type: "string",
+      demandOption: false,
+      describe: "Optional dataset identifier for this import",
+      default: getDefaultDatasetId(),
+    })
+    .help().argv;
 
-  await parseAndLoadBrainvarData({
-    geneMapTsv,
-    cpmTsv,
-    metaTsv,
-    datasetId: DATASET_ID,
-  });
+  const GENE_MAP_FILE = argv["gene-map"];
+  const CPM_FILE = argv["cpm"];
+  const META_FILE = argv["meta"];
+  const DATASET_ID = argv["dataset-id"];
+
+  const db = knex(dbConfig[process.env.NODE_ENV || "development"]);
+
+  try {
+    const geneMapTsv = await readGzippedTsvFile(GENE_MAP_FILE);
+    const cpmTsv = await readTsvFile(CPM_FILE);
+    const metaTsv = await readTsvFile(META_FILE);
+
+    await parseAndLoadBrainvarData({
+      db,
+      geneMapTsv,
+      cpmTsv,
+      metaTsv,
+      datasetId: DATASET_ID,
+    });
+  } finally {
+    await db.destroy();
+  }
 }
 
-await run();
+try {
+  await run();
+  console.log("BrainVar ETL process from file system completed successfully.");
+  process.exit(0);
+} catch (error) {
+  console.error("Error occurred while running ETL process:", error);
+  process.exit(1);
+}

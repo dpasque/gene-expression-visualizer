9c45da7ec91b2c5e7582a7096857fc84395b539d
Dan Speckhard-Pasque <pasquedan@gmail.com>
2025-07-02 20:41:12 -0500

    Style tweak

--- client/src/components/AppMain.tsx
+++ client/src/components/AppMain.tsx
@@ -49,7 +49,7 @@ export function AppMain() {
   }, []);
 
   return (
-    <main className="flex-1 border-x border-gray-200 max-w-5xl mx-auto min-h-75 px-4 sm:px-8 md:px-12 pt-6 pb-12">
+    <main className="flex-1 border-x border-gray-200 w-full max-w-5xl mx-auto min-h-75 px-4 sm:px-8 md:px-12 pt-6 pb-12">
       {staticDataLoading && (
         <div className="flex flex-col items-center gap-y-4" role="status">
           <LoadingSpinner />
